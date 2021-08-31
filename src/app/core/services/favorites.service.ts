3bf656d2c96b7dbf3e8025d962481dba77dc1c2a
kafkaesc <jared.a.hettinger@gmail.com>
2021-08-30 23:04:21 -0500

    Updating short stories to pull from service

--- src/app/core/services/favorites.service.ts
+++ src/app/core/services/favorites.service.ts
@@ -13,58 +13,8 @@ export class FavoritesService {
     constructor(private http: HttpClient) { }
 
     favDevSites: ISiteFavorite[] | null = null;
+    favShortStories: IFictionFavorite[] | null = null;
     favYouTubeChannels: IYouTubeFavorite[] | null = null;
-
-    favShortStories: IFictionFavorite[] = [
-        {
-            id: 901,
-            title: 'The Library of Babel',
-            author: 'Jorge Luis Borges',
-            type: 'short story',
-            goodreadsUrl: 'https://www.goodreads.com/book/show/172366.The_Library_of_Babel',
-            publicationYear: 1941
-        },
-        {
-            id: 902,
-            title: 'Barn Burning',
-            author: 'William Faulkner',
-            type: 'short story',
-            goodreadsUrl: 'https://www.goodreads.com/book/show/3120865-barn-burning',
-            publicationYear: 1939
-        },
-        {
-            id: 903,
-            title: 'Young Goodman Brown',
-            author: 'Nathaniel Hawthorne',
-            type: 'short story',
-            goodreadsUrl: 'https://www.goodreads.com/book/show/562471.Young_Goodman_Brown',
-            publicationYear: 1835
-        },
-        {
-            id: 904,
-            title: 'The Judgment',
-            author: 'Franz Kafka',
-            type: 'short story',
-            goodreadsUrl: 'https://www.goodreads.com/book/show/16244632-the-judgment',
-            publicationYear: 1913
-        },
-        {
-            id: 905,
-            title: 'Recitatif',
-            author: 'Toni Morrison',
-            type: 'short story',
-            goodreadsUrl: 'https://www.goodreads.com/en/book/show/34842610-recitatif',
-            publicationYear: 1983
-        },
-        {
-            id: 906,
-            title: 'The Oblong Box',
-            author: 'Edgar Allan Poe',
-            type: 'short story',
-            goodreadsUrl: '',
-            publicationYear: 1844
-        }
-    ];
 
     /* Dev Site Favorites */
     getFavoriteDevSites(): ISiteFavorite[] | null {
@@ -93,12 +43,15 @@ export class FavoritesService {
     }
 
     /* Short Story Favorites */
-    getFavoriteShortStories(): IFictionFavorite[] {
+    getFavoriteShortStories(): IFictionFavorite[] | null {
         return this.favShortStories;
     }
 
     pullFavoriteShortStories(): Observable<any> {
         let uri: string = './assets/data/favorite-short-stories.json';
+        this.http.get<IFictionFavorite[] | null>(uri).subscribe(
+            res => this.favShortStories = res
+        );
         return this.http.get<IFictionFavorite[] | null>(uri);
     }
 
@@ -106,7 +59,7 @@ export class FavoritesService {
      * (Can be short story, novella, novel, etc.) */
     getFictionFavoriteById(id: number): IFictionFavorite | null {
         let result: IFictionFavorite | null = this.favShortStories
-            .filter(sh => sh.id === id)
+            ?.filter(sh => sh.id === id)
             .shift() || null;
         return result;
     }
--- src/app/favorites/short-stories/short-stories.component.ts
+++ src/app/favorites/short-stories/short-stories.component.ts
@@ -8,11 +8,17 @@ import { FavoritesService } from 'src/app/core/services/favorites.service';
     styleUrls: ['./short-stories.component.css']
 })
 export class ShortStoriesComponent implements OnInit {
-    favList!: IFictionFavorite[];
+    favList: IFictionFavorite[] | null = null;
 
     constructor(private favoriteService: FavoritesService) { }
 
     ngOnInit(): void {
         this.favList = this.favoriteService.getFavoriteShortStories();
+        if (!this.favList) {
+            this.favoriteService.pullFavoriteShortStories().subscribe(
+                res => this.favList = res,
+                err => console.log('Error pulling favorite short stories: ', err)
+            );
+        }
     }
 }
